9e92f20c79ff676c0947df1e6acc03977a26c511
alber <alber@ALBERTO_BARBOSA>
2022-08-12 09:52:32 -0300

    Adicionar arquivos de projeto.

---
+++ Salario/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+
+namespace Salario;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        CultureInfo CI = CultureInfo.InvariantCulture;
+        double salario, reajuste, percentual, NovoSalario;
+
+        Console.WriteLine("Digite o salario atual do funcionario");
+        salario = double.Parse(Console.ReadLine());
+        Console.WriteLine("Agora por favor, digite o percentual de reajuste deste salario");
+        percentual = double.Parse(Console.ReadLine());
+
+        reajuste = salario / 100 * percentual;
+        NovoSalario = reajuste + salario;
+
+        Console.WriteLine("O salario reajustado deste funcionario é de R$" + NovoSalario.ToString("F2", CI));
+    }
+}
